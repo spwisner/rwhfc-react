aeb37225365803f6b4e2db7f4c18e37af9d361c6
Stephen Wisner <spwisner@gmail.com>
2018-02-27 23:46:12 -0500

    Progress

--- src/containers/latest/Latest.js
+++ src/containers/latest/Latest.js
@@ -9,7 +9,9 @@ export default class Latest extends React.Component {
       <div>
         <Row className="white-background">
         <Col xs={12} sm={6}>
-          <h2 className="message-header">Register to Vote</h2>
+          <h2 className="message-header">I'm on the Ballot</h2>
+          <p className="register">I am on the ballot as a Democratic Candidate
+          for the US House of Representatives from the MO 2nd, as of earlier today.</p>
           <p className="register">If you haven't already done so, <a href="https://www.sos.mo.gov/elections/goVoteMissouri/register" target="_blank" rel="noopener noreferrer">register to vote</a> and vote!.  Almost half of us didn't bother to cast a ballot in 2016 and look what that gave us!  We need everyone, who might Vote Blue, to come to polls, especially those of us in the Missouri 2nd!</p>
         </Col>
           <Col xs={12} sm={6}>
